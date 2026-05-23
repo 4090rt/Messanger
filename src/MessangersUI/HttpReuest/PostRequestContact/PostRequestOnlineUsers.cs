0fa26429184d04e05a9fc44088f90e12a67c6b2c
4090rt <artem2007yannurow@gmail.com>
2026-05-23 18:18:30 +0500

    Add files via upload

---
+++ src/MessangersUI/HttpReuest/PostRequestContact/PostRequestOnlineUsers.cs
@@ -0,0 +1,109 @@
+﻿using Messangers.ModelData;
+using MessangersUI.Delegate;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MessangersUI.HttpReuest.PostRequestContact
+{
+    public class PostRequestOnlineUsers
+    {
+        private readonly ILogger<PostRequestOnlineUsers> _logger;
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly ExceptionDelegate _exceptionDelegate;
+        private readonly HttpExceptionDelegate _httpExceptionDelegate;
+        private readonly JsonExceptionDelegate _jsonExceptionDelegate;
+        private readonly TaskCanccelException _taskCanccelException;
+
+        public PostRequestOnlineUsers(ILogger<PostRequestOnlineUsers> logger, IHttpClientFactory httpClientFactory, ExceptionDelegate exceptionDelegate,
+            HttpExceptionDelegate httpExceptionDelegate, JsonExceptionDelegate jsonExceptionDelegate, TaskCanccelException taskCanccelException)
+        {
+            _logger = logger;
+            _httpClientFactory = httpClientFactory;
+            _exceptionDelegate = exceptionDelegate;
+            _httpExceptionDelegate = httpExceptionDelegate;
+            _jsonExceptionDelegate = jsonExceptionDelegate;
+            _taskCanccelException = taskCanccelException;
+        }
+
+        public async Task<List<DataUsersList>> RequestPost(List<DataUsersList> list)
+        {
+            try
+            {
+                var client = _httpClientFactory.CreateClient("Client1Http2.0");
+
+                var options = new HttpRequestMessage(HttpMethod.Post, "https://localhost:7167/api/ControllerOnlineUser/onlinetusers")
+                {
+                    Version = HttpVersion.Version20,
+                    VersionPolicy = HttpVersionPolicy.RequestVersionOrHigher
+                };
+
+                if (list != null)
+                {
+                    var users = list.FirstOrDefault();
+
+                    var modeltosend = new
+                    {
+                        User = users?.User
+                    };
+
+                    var jsonser = JsonSerializer.Serialize(modeltosend, new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+
+                    var stringcontent = new StringContent(jsonser, Encoding.UTF8, "application/json");
+
+                    options.Content = stringcontent;
+
+                    HttpResponseMessage response = await client.SendAsync(options).ConfigureAwait(false);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var resultresponce = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+                        var jsondoc = JsonDocument.Parse(resultresponce);
+                        var root = jsondoc.RootElement;
+
+                        var listresult = JsonSerializer.Deserialize<List<DataUsersList>>(root.GetProperty("message"));
+
+                        return listresult;
+                    }
+                    else
+                    {
+                        _logger.LogError("Запрос неудача" + response.StatusCode);
+                        return new List<DataUsersList>();
+                    }
+                }
+                else
+                {
+                    return new List<DataUsersList>();
+                }
+            }
+            catch (TaskCanceledException ex)
+            {
+                await _taskCanccelException.RunDelegate(_taskCanccelException.Delegate, ex);
+                return new List<DataUsersList>();
+            }
+            catch (HttpRequestException ex)
+            {
+                await _httpExceptionDelegate.RunDelegate(_httpExceptionDelegate.Delegate, ex);
+                return new List<DataUsersList>();
+            }
+            catch (JsonException ex)
+            {
+                await _jsonExceptionDelegate.RunDelegate(_jsonExceptionDelegate.Delegate, ex);
+                return new List<DataUsersList>();
+            }
+            catch (Exception ex)
+            {
+                await _exceptionDelegate.RunDelegate(_exceptionDelegate.DelegateException, ex);
+                return new List<DataUsersList>();
+            }
+        }
+    }
+}
---
+++ src/MessangersUI/main.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MessangersUI
+{
+    /// <summary>
+    /// Логика взаимодействия для main.xaml
+    /// </summary>
+    public partial class main : Window
+    {
+        public main()
+        {
+            InitializeComponent();
+
+        }
+
+        ////private void MessageTextBox_PreviewKeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
+        ////{
+        ////    if (e.KeyValue == Key.Enter && Keyboard.Modifiers != ModifierKeys.Shift)
+        ////    {
+        ////        e.Handled = true;
+        ////        SendMessage();
+        ////    }
+        //}
+
+        private void MessageTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var textBox = sender as System.Windows.Controls.TextBox;
+
+            // Автоматическая прокрутка к курсору
+            textBox?.ScrollToEnd();
+
+            // Обновляем размер скроллвьюера
+            if (textBox?.LineCount > 3)
+            {
+                MessageScrollViewer.UpdateLayout();
+            }
+        }
+    }
+}
